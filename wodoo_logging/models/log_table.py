faf691e0c8c995ef7191e78f1789a20ac1764cfa
Marc Wimmer <marc@zebroo.de>
2024-04-27 08:25:45 +0200

    main: Fixed

--- wodoo_logging/models/log_table.py
+++ wodoo_logging/models/log_table.py
@@ -1,14 +1,56 @@
+import os
+import psycopg2
 from odoo import _, api, fields, models, SUPERUSER_ID
 from odoo.exceptions import UserError, RedirectWarning, ValidationError
+
 
 class LogTable(models.Model):
     _name = "logging.data"
 
     date = fields.Datetime()
-    level = fields.Selection([
-        ('INFO', 'INFO'),
-        ('ERROR', 'ERROR'),
-        ('WARN', 'WARN'),
-        ('DEBUG', 'DEBUG'),
-    ])
+    level = fields.Selection(
+        [
+            ("INFO", "INFO"),
+            ("ERROR", "ERROR"),
+            ("WARN", "WARN"),
+            ("DEBUG", "DEBUG"),
+        ]
+    )
     line = fields.Char()
+
+    @api.model
+    def _fetch_logs(self):
+        conn = self._get_conn()
+        cr = conn.cursor()
+        try:
+
+            cr.execute(
+                """
+                select id, date, ttime, line, level from console_log where 
+                coalesce(exported, false) = false order by id desc
+                limit 20000
+                """
+            )
+
+            for rec in cr.fetchall():
+                id, date, ttime, line, level = rec
+
+                cr.execute(
+                    """update console_log
+                    set exported=true
+                    where id = %s
+                    """,
+                    (id,),
+                )
+
+        finally:
+            conn.close()
+
+    @api.model
+    def _get_conn(self):
+        psycopg2.connect(
+            host="fluentd_postgres",
+            username="fluentd",
+            password="fluentd",
+            dbname="fluentd",
+        )
