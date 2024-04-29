1e5fc25b9e541cdc811abb48febe51e91c0c30a7
Marc Wimmer <marc@zebroo.de>
2024-04-29 10:59:18 +0200

    main: Fixed

--- wodoo_logging/models/log_table.py
+++ wodoo_logging/models/log_table.py
@@ -11,7 +11,7 @@ from odoo.tools import DEFAULT_SERVER_DATE_FORMAT
 
 class LogTable(models.Model):
     _name = "logging.data"
-    _order = "extid desc"
+    _order = "date desc"
     _log_access = False
 
     extid = fields.Integer(index=True)
@@ -35,8 +35,10 @@ class LogTable(models.Model):
 
     @api.model
     def _parse_line(self, line):
+        line = line or ''
         for level in [x[0] for x in self._fields["level"].selection]:
             if f" {level.upper()}" in line:
+                line = line.replace(level, "")
                 break
         else:
             level = False
@@ -44,11 +46,13 @@ class LogTable(models.Model):
         match = re.findall(r"\d\d\d\d-\d\d-\d\d \d\d:\d\d:\d\d", line)
         if match:
             date = match[0]
+            line = line.replace(match[0], "")
         else:
             date = False
 
         for method in ["GET", "POST"]:
             if f" {method} " in line:
+                line = line.replace(method, "")
                 break
         else:
             method = False
@@ -59,36 +63,41 @@ class LogTable(models.Model):
         else:
             url = False
 
-        return date, level, method, url
+        line = self._make_nice_line(line)
+
+        return date, level, method, url, line
+
+    @api.model
+    def _make_nice_line(self, line):
+        line = line.strip()
+        line = line.split("?")
+        if len(line) > 1:
+            if len(line[0]) < 10:
+                line = line[1]
+        else:
+            line = line[0]
+        return line
 
     @api.model
     def _fetch_logs(self):
         with self._get_conn() as (conn, cr):
             cr.execute(
                 """
-                select id, line, tag from console_log where 
-                coalesce(exported, false) = false 
+                select id, line, tag from console_log 
                 order by id desc
-                limit 20000
                 """
             )
 
             for rec in cr.fetchall():
                 id, line, tag = rec
-                date, level, method, url = self._parse_line(line)
+                date, level, method, url, line = self._parse_line(line)
 
-                cr.execute(
-                    """delete from console_log
-                    where id = %s
-                    """,
-                    (id,),
-                )
                 if not self.search_count([("extid", "=", id)]):
                     self.create(
                         {
                             "extid": id,
                             "level": level or False,
-                            "date": date,
+                            "date": date or fields.Datetime.now(),
                             "line": line,
                             "tag": tag,
                             "method": method,
@@ -96,6 +105,12 @@ class LogTable(models.Model):
                         }
                     )
                     self.flush()
+                cr.execute(
+                    """delete from console_log
+                    where id = %s
+                    """,
+                    (id,),
+                )
                 self.env.cr.commit()
                 conn.commit()
 
