f8cb1be281348481f1e9ba955dbf9a7643f857ed
Marc Wimmer <marc@zebroo.de>
2024-04-29 16:05:56 +0200

    main: Fixed

--- wodoo_logging/models/log_table.py
+++ wodoo_logging/models/log_table.py
@@ -14,6 +14,7 @@ class LogTable(models.Model):
     _order = "date desc"
     _log_access = False
 
+    orig_line = fields.Char("Orig Line")
     extid = fields.Integer(index=True)
     tag = fields.Char("Container")
     date = fields.Datetime()
@@ -34,8 +35,10 @@ class LogTable(models.Model):
     ]
 
     @api.model
-    def _parse_line(self, line):
-        line = line or ''
+    def _parse(self):
+        line = self.orig_line or ''
+        if not line:
+            return
         for level in [x[0] for x in self._fields["level"].selection]:
             if f" {level.upper()}" in line:
                 line = line.replace(level, "")
@@ -51,60 +54,79 @@ class LogTable(models.Model):
             date = False
 
         for method in ["GET", "POST"]:
-            if f" {method} " in line:
+            if f"{method} " in line:
                 line = line.replace(method, "")
                 break
         else:
             method = False
 
-        match = re.findall(r"/\ \/[^\ ]*", line)
+        match = re.findall(r"\ (\/[^\ ]*)", line)
         if match:
             url = match[0]
+            line = line.replace(url, "")
         else:
             url = False
 
         line = self._make_nice_line(line)
 
-        return date, level, method, url, line
+        vals = {
+            "level": level or False,
+            "date": date or fields.Datetime.now(),
+            "line": line,
+            "method": method,
+            "url": url,
+        }
+        self.write(vals)
 
     @api.model
     def _make_nice_line(self, line):
         line = line.strip()
+        if line.startswith(","):
+            line = line.lstrip(",")
         line = line.split("?")
         if len(line) > 1:
             if len(line[0]) < 10:
                 line = line[1]
         else:
             line = line[0]
+
+        # 349 102 dbname longpolling
+        if self.env.cr.dbname in line:
+            line = line.split(self.env.cr.dbname)
+            for c in "0123456789":
+                line[0] = line[0].replace(c, "")
+            line = self.env.cr.dbname.join(line).strip()
         return line
+
+
 
     @api.model
     def _fetch_logs(self):
         with self._get_conn() as (conn, cr):
             cr.execute(
                 """
-                select id, line, tag from console_log 
+                select id, line, tag, col_datetime from console_log 
                 order by id desc
                 """
             )
 
             for rec in cr.fetchall():
-                id, line, tag = rec
-                date, level, method, url, line = self._parse_line(line)
+                id, line, tag, date = rec
+                orig_line = line
+                if not date:
+                    date = date or fields.Datetime.now()
 
                 if not self.search_count([("extid", "=", id)]):
-                    self.create(
+                    line = self.create(
                         {
                             "extid": id,
-                            "level": level or False,
+                            "orig_line": orig_line,
                             "date": date or fields.Datetime.now(),
-                            "line": line,
                             "tag": tag,
-                            "method": method,
-                            "url": url,
                         }
                     )
                     self.flush()
+                    line._parse()
                 cr.execute(
                     """delete from console_log
                     where id = %s
